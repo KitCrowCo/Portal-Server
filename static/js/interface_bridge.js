fb4b08fb095a86e06344b6ea5e5832ec7fcb7977
JoshuaTanner <jetanner@uci.edu>
2026-08-28 16:47:11 -0700

    Automated sync node update

--- static/js/interface_bridge.js
+++ static/js/interface_bridge.js
@@ -304,7 +304,7 @@ const IB = (() => {
             case 'cfg': { Object.assign(_cfg, d.values || {}); _vpSetActive(_cfg.pointer); break; }
 
             case 'pipeline_event': { document.dispatchEvent(new CustomEvent('pipeline:' + d.event, { detail: { job_id: d.job_id, ...d.payload } })); break; }
-            case 'pipeline_stream': { document.dispatchEvent(new CustomEvent('pipeline:stream', { detail: { job_id: d.job_id, key: d.key, delta: d.delta } })); break; }
+            case 'pipeline_stream': { document.dispatchEvent(new CustomEvent('pipeline:stream', { detail: { job_id: d.job_id, node: d.node, key: d.key, delta: d.delta } })); break; }
         }
     }
     // -- Component Generation --
